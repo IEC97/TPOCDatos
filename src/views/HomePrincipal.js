8b7b27db4875e7c20a34d83eb43ec5a3c9108f5b
Ivana Cardozo <iva6869@gmail.com>
2024-10-23 12:10:06 -0300

    eliminando archivos

--- src/views/HomePrincipal.js
+++ src/views/HomePrincipal.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 // import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { BrowserRouter as Router, Route, Navigate } from "react-router-dom";
-import Home from "../App";
-import Login from "../App1";
+//import Home from "../App";
+//import Login from "../App1";
 //import VistaAdmin from './VistaAdmin';
 //import VistaJugador from './VistaJugador';
 
@@ -14,13 +14,13 @@ class App extends Component {
 			<Router>
 				<Route path="/login/" component={Login} />
 				<Route path="/home/" component={Home} />
-				{/* <Route path="/VistaAdmin/" component={VistaAdmin} />	
-				<Route path="/VistaJugador/" component={VistaJugador} /> */}						 
+				<Route path="/VistaAdmin/" component={VistaAdmin} />	
+				<Route path="/VistaJugador/" component={VistaJugador} /> 	 					 
 				<Route exact path="/" render={() => <Navigate to="/home/"/>} />
 
 			</Router>
 		)
-	}
+	 }
 }
 
 export default App;
