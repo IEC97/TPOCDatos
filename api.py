15ab7e380f76025a732dd8b19c9727cd799e1672
Ivana Cardozo <iva6869@gmail.com>
2024-11-03 17:29:06 -0300

    Copiando otra vez los archivos

--- api.py
+++ api.py
@@ -2,13 +2,14 @@ from flask import Flask, request , render_template ,jsonify
 import joblib
 import pandas as pd
 import numpy as np
+
 app = Flask(__name__)
 model = joblib.load("knn_model.pkl")
 label_encoder = joblib.load('label_encoder.pkl')
 
 @app.route('/',methods=['GET'])
 def form():
-    return render_template('VistaAdmin.js')
+    return render_template('form.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -22,7 +23,7 @@ def predict():
     features = np.array([[new_genre_encoded, hours]])
     print(features)
     prediction = model.predict(pd.DataFrame(features))
-    return render_template("VistaAdmin.js" , prediction=prediction)
+    return render_template("form.html" , prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
